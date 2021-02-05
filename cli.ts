5f1074a4d96a13fc4d0d541d3b418a80c959428d
Eyoatam <eyoatamtamirat7@gmail.com>
2021-02-05 21:38:44 +0300

    refractor(cli): move gif creation to a new functon

--- cli.ts
+++ cli.ts
@@ -4,18 +4,31 @@ import { commandTs } from "./deps/mod.ts";
 import { gif } from "./mod.ts";
 import { Options } from "./types/types.ts";
 
-await new commandTs.Command()
-  .name("gif")
-  .version("0.2.5")
-  .description("A command line tool for creating gifs")
-  .option("-w, --width <width:number>", "max width [500]")
-  .option("-l, --height <height:number>", "max height [none]")
-  .arguments("[inputfile:string] [outputfile: string]")
-  .action((options: Options, inputfile: string, outputfile: string) => {
-    const opts = {
-      width: options.width,
-      height: options.height,
-    };
-    gif(inputfile, outputfile, opts);
-  })
-  .parse(Deno.args);
+await cli();
+async function cli() {
+	await new commandTs.Command()
+		.name("gif")
+		.version("0.2.8")
+		.description("A command line tool for creating gifs")
+		.option("-w, --width <width:number>", "default 480")
+		.option("-l, --height <height:number>", "height 380")
+		.arguments("[inputfile:string] [outputfile: string]")
+		.action(createGif())
+		.parse(Deno.args);
+
+	// deno-lint-ignore no-explicit-any
+	function createGif(): commandTs.IAction<any, any> {
+		return (options: Options, inputfile: string, outputfile: string) => {
+			if (Deno.args.length < 2) {
+				console.log(`Expected 2 arguments but got ${Deno.args.length}
+      type '--help' to output help`);
+			} else {
+				const opts = {
+					width: options.width,
+					height: options.height,
+				};
+				gif(inputfile, outputfile, opts);
+			}
+		};
+	}
+}
