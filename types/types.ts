11e2b674ffa448c3c8991e0a9b540bca727f3573
Eyoatam Tamirat <eyoatamtamirat7@gmail.com>
2021-02-10 19:49:42 +0300

    feat(cli): added support for conversion of video files to mp3, mp4, avi and webm (#10)

--- types/types.ts
+++ types/types.ts
@@ -1,6 +1,15 @@
 export interface Options {
   width: number;
   height: number;
+  mp3?: string;
+  mp4?: string;
+  avi?: string;
+  webm?: string;
+}
+
+export interface CLiOptions {
+  input: string;
+  output: string;
 }
 
 export interface EncodingProgress {
