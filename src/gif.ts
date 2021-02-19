11a5eff7fef8df81df8ed3fc94cf2053e1f109d0
Eyoatam <eyoatamtamirat7@gmail.com>
2021-02-19 19:33:43 +0300

    refractor(gif): added options for configuring output file path

--- src/gif.ts
+++ src/gif.ts
@@ -1,6 +1,60 @@
 import { colorsTs, ffmpegTs } from "../deps/mod.ts";
 import { EncodingProgress, Options } from "../types/types.ts";
 
+// Handlers
+
+function validateOutput(output: string) {
+  const splitOutput: Array<string> = output.split("");
+  const splicedOutput: Array<string> = splitOutput.splice(2, output.length - 2);
+  if (splicedOutput[0] === ".") {
+    throw new Error("Invalid path");
+  }
+  if (splicedOutput.includes(".")) {
+    const error = new Error(
+      `unexpected character '.' output file name must not contain a '.' or extension like '.mp4', '.gif'`,
+    );
+    throw error;
+  }
+  if (splitOutput[0] === "/") {
+    const error = new Error(
+      `unexpected character '/' output file name must not contain a '/' or extension like '.mp4', '.gif'`,
+    );
+    throw error;
+  }
+}
+
+function checkForInputAndOutput(input: string, output: string) {
+  if (!input) {
+    const error = new Error("input file is required");
+    throw error;
+  }
+
+  if (!output) {
+    const error = new Error("output file is required");
+    throw error;
+  }
+}
+
+function handleProgress(event: EncodingProgress) {
+  if (!event.frame && !event.outTimeMs && !event.fps && !event.speed) {
+    console.log(colorsTs.green("[ffmpeg]: ") + `progress ${event.progress}`);
+  }
+  if (!event.fps && !event.frame) {
+    console.log(
+      colorsTs.green("[ffmpeg]: ") +
+        `time: ${event.outTimeMs}ms speed: ${event.speed}x`,
+    );
+  }
+  if (event.done) {
+    console.log(`✨Done✨ in ${event.outTimeMs} ms`);
+  } else {
+    console.log(
+      colorsTs.green("[ffmpeg]: ") +
+        `frame: ${event.frame} fps: ${event.fps} time: ${event.outTimeMs}ms speed: ${event.speed}x`,
+    );
+  }
+}
+
 /**
  * convert `input` video files to `.gif` with the given `options`
  * @param input @type string
@@ -130,52 +184,4 @@ export function webm(input: string, output: string, options?: Options) {
     .height(options ? options?.height : 380)
     .output("./" + output + ".webm")
     .encode();
-}
-
-function validateOutput(output: string) {
-  const splitOutput: Array<string> = output.split("");
-  if (splitOutput.includes(".")) {
-    const error = new Error(
-      `unexpected character '.' output file name must not contain a '.' or extension like '.mp4', '.gif'`,
-    );
-    throw error;
-  }
-  if (splitOutput.includes("/")) {
-    const error = new Error(
-      `unexpected character '/' output file name must not contain a '/' or extension like '.mp4', '.gif'`,
-    );
-    throw error;
-  }
-}
-
-function checkForInputAndOutput(input: string, output: string) {
-  if (!input) {
-    const error = new Error("input file is required");
-    throw error;
-  }
-
-  if (!output) {
-    const error = new Error("output file is required");
-    throw error;
-  }
-}
-
-function handleProgress(event: EncodingProgress) {
-  if (!event.frame && !event.outTimeMs && !event.fps && !event.speed) {
-    console.log(colorsTs.green("[ffmpeg]: ") + `progress ${event.progress}`);
-  }
-  if (!event.fps && !event.frame) {
-    console.log(
-      colorsTs.green("[ffmpeg]: ") +
-        `time: ${event.outTimeMs}ms speed: ${event.speed}x`,
-    );
-  }
-  if (event.done) {
-    console.log(`✨Done✨ in ${event.outTimeMs} ms`);
-  } else {
-    console.log(
-      colorsTs.green("[ffmpeg]: ") +
-        `frame: ${event.frame} fps: ${event.fps} time: ${event.outTimeMs}ms speed: ${event.speed}x`,
-    );
-  }
 }
