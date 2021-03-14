cbe3c6b3ade8d12c79ede5aa4784584501d93d07
Eyoatam <eyoatamtamirat7@gmail.com>
2021-03-14 13:19:21 +0300

    chore(deps): bump deno_std to 0.90.0

--- deps/mod.ts
+++ deps/mod.ts
@@ -1,3 +1,3 @@
 export * as ffmpegTs from "https://deno.land/x/fast_forward@0.1.1/ffmpeg.ts";
-export * as colorsTs from "https://deno.land/std@0.86.0/fmt/colors.ts";
-export * as commandTs from "https://deno.land/x/cliffy/command/mod.ts";
+export * as colorsTs from "https://deno.land/std@0.90.0/fmt/colors.ts";
+export * as commandTs from "/https://deno.land/x/cliffy@v0.18.0/command/mod.ts";
