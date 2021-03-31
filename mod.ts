0687813c6bef835e36ba3bac0204d633a0d77432
Eyoatam <eyoatamtamirat7@gmail.com>
2021-03-31 16:03:20 +0300

    export functions

--- mod.ts
+++ mod.ts
@@ -1,2 +1,3 @@
-export * from "./typedef/gif.d.ts";
+export { avi, gif, mov, mp3, mp4, webm } from "./src/gif.ts";
 export type { EncodingProgress, Options } from "./src/gif.ts";
+export * from "./typedef/gif.d.ts";
