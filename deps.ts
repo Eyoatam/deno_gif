cac4a9ec196c0925cb7797e8bf68f8ebf9931abe
Eyoatam <eyoatamtamirat7@gmail.com>
2021-05-04 20:03:30 +0300

    upgrade deno_std to 0.95.0

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export * as ffmpegTs from "https://deno.land/x/fast_forward@0.1.3/ffmpeg.ts";
-export * as colorsTs from "https://deno.land/std@0.93.0/fmt/colors.ts";
+export * as colorsTs from "https://deno.land/std@0.95.0/fmt/colors.ts";
 export * as commandTs from "https://deno.land/x/cliffy@v0.18.2/command/mod.ts";
